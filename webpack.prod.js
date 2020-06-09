b810905ed5d7c79741094a9f8d5d03cbb5e0da0d
StephenChips <tcw6991@gmail.com>
2020-06-09 12:33:21 +0800

    Changes as follow:
    1. Added BundleAnalyzerPlugin
    
    2. Deleted useless /source folder
    
    3. update highlight.js and bootstrap.js CDN to BootCDN

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
     mode: 'production',
@@ -32,7 +33,8 @@ module.exports = {
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: './index.html'
-        })
+        }),
+        new BundleAnalyzerPlugin()
     ],
     externals: {
       'jQuery': 'jQuery',
