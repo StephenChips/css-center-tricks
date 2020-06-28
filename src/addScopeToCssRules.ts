646de57c1cb94033b8fea64b30c3411fbbbba3e7
StephenChips <tcw6991@gmail.com>
2020-06-28 20:58:21 +0800

    [WIP] finish addScopeToCssRules.js
    1. Adjust the function `addScopeToCssRules`, so that it can accept and
       process data from CSSRuleSetParser.
    
    2. finish the function `getAnimationName`, which can extract animation
       name from shorthand value of `animation` property. e.g. extra
       `fadeout` from `animation: fadeout 2s;`, which the shorthand value is
       `fadeout 2s`.
    
    [TODO]
    1. Finish the function that transform CSS data back to string.
    2. Test staffs.

--- src/addScopeToCssRules.ts
+++ src/addScopeToCssRules.ts
@@ -1,346 +1,236 @@
-/**
-Add a "scope" to given CSS rules by appending an extra selector
-at front. It limit the effective range of each CSS rule to curtain
-elements, prevent it from overwrite some irrelative rules, causing
-some quirky style problem.
- */
-export function addScopeToCssRules (extraSelector, rules) {
-    let parser = new SimpleCSSParser(rules);
-    let ast = parser.parse();
-
-    for (var rule of ast) {
-        // Ignore CSS At-Rules, e.g. @keyframes, @media.
-        if (rule.selectorGroup.startsWith('@')) {
-            continue;
-        } else {
-            rule.selectorGroup = extraSelector + ' ' + rule.selectorGroup;
-        }
-    }
-
-    return assembleSelectorsAndRules(ast);
-}
+import { CSSRuleSetParser, Rule, KeyframeAtRule } from './CSSRuleSetParser.js';
 
 /**
- * ### Short Introduction of CSS Syntax
- * 
- * A CSS rule is comprised of two part, the selector group and
- * the declaration block.
- * 
- * Take a simple CSS rule for example:
- * ```
- * div.flex, p.flex { display: flex; }
- * ```
- * 
- * In this case, the selector group is the part
- * `div.flex, p.flex`. Obvioiusly it consist of a chain of CSS
- * selectors, and the rest, the braces and the contents wrapped
- * inside it, is the declaration block, which defines CSS
- * properties and values.
+By adding a scope, we limit the effective range of each CSS rule,
+prevent it from effect some irrelative css rules that causes some
+quirky style problem.
+
+Technically. We add a class in front of each selector, and change
+each animation names.
  */
+export function addScopeToCssRules (scope : string, css : string) {
+    let parser = new CSSRuleSetParser(css);
+    let ruleSet : Rule[] = parser.parse();
 
-// The CSS parser outputs type type of rules. Each rule's has their own type
-// and are described below.
-type Rule = CSSRule | KeyframeAtRule | RuleSetAtRule;
+    addScope(scope, ruleSet);
 
-// The first one, which is the commonest one, is the CSSRule. It contains a set of CSS
-// decalarations, each of which declares a CSS property and a value.
-// For example:
-// ```
-// div { display: block; }
-// ```
-// is a CSSRule.
-type CSSRule = {
-    type: 'cssRule',
-    selectors: string[],
-    decalrations: { property: string, value: string }[]
-};
-// Following type stands for the '@keyframes' At-Rule. After parsing,
-// You will get the name and the decalaration body.
-//
-// For example, if you have a keyframes At-Rule:
-// @keyframes fadeout { from { opacity: 1; } to { opacity: 0; } }
-//
-// After parsing, you should have a CSSRule object like this:
-// { type: '@keyframes', name: 'fadeout', declr: '{ from { opacity: 1; } to { opacity: 0; } }' }
-//
-// Notice that all blanks and return characters in the decalaration body will be reserved. \
-type KeyframeAtRule = {
-    type: '@keyframes',
-    name: string,
-    declr: string;
-};
-
-
-// Following type stands for the '@media', '@support' and '@document' At-Rules.
-// Each of these three At-Rules has an nested rule set, making the `Rule` type
-// a recursive data type.
-type RuleSetAtRule = {
-    type: '@media' | '@support',
-    conditions: string,
-    rules: Rule[]
-};
-
-
-function parseRuleSet () {
-
+    return assembleSelectorsAndRules(ruleSet);
 }
 
-function parseKeyframeAtRule () {
+function addScope (scope : string, ruleSet : Rule[]) {
+    type Decalaration = {
+        property: string;
+        value: string;
+    };
 
-}
+    // This map will store every "animation" & "animaiton-name"
+    // property we encounter during the iteration. Every properties
+    // are grouped by its animation name.
+    let animationMap = new Map<string, Decalaration[]>();
 
-function parseCSSRule () {
+    // This map will store every @keyframes we encounter during
+    // the iteration.
+    let keyframesMap = new Map<string, KeyframeAtRule>();
 
-}
+    for (let rule of ruleSet) {
+        if (rule.type === 'cssRule') {
+            rule.selectors = rule.selectors.map(sel => `[data-${scope}] ${sel}`);
 
-function skipVoidAtRules () {
+            for (let declr of rule.decalrations) {
+                let animationName : string;
+                if (declr.property === 'animation') {
+                    animationName = getAnimationName(declr.value);
+                } else if (declr.property === 'animation-name') {
+                    animationName = declr.value.trim();
+                }
 
-}
+                let listOfDeclr = animationMap.get(animationName);
+                if (listOfDeclr === undefined) {
+                    listOfDeclr = [];
+                    animationMap.set(animationName, listOfDeclr);
+                }
 
-function skipNestedBraces () {
-
-}
-
-/**
- * This parser is tailor-make for the function addScopeToCssRule.
- * And thus should not be public.
- */
-class SimpleCSSParser {
-    private rules : string;
-    private cursor : number = 0;
-    private result : null | Rule[] = null;
-
-    constructor (rules : string) {
-        this.rules = rules;
-    }
-
-    parse () {
-        if (this.result !== null) {
-            return this.result;
-        }
-
-        this.result = [];
-        
-        // We make an assumption that the input CSS rules are correct. It neither
-        // has any syntax errors, nor has any sematic errors (e.g. unknown properties,
-        // illegal units).
-
-        return this._parseRuleSet();
-    }
-
-    _parseRuleSet () : Rule[] {
-        this._skipWhitespacesAndBreaks();
-
-        /**
-         * There are two situations shows the parsing should be terminated.
-         * 1. Meet the end of the string
-         * 2. Meet the character '}' 
-         * 
-         * // TODO
-         */
-        while (this.cursor < this.rules.length && this.rules[this.cursor] !== '}') {
-            let rule : Rule = this._parseRule();
-            this.result.push(rule);
-            this._skipWhitespacesAndBreaks();
-        }
-
-        return this.result;
-    }
-
-    _skipWhitespacesAndBreaks () {
-        while (this.cursor < this.rules.length && this._isWhitespaceOrBreak(this.rules[this.cursor])) {
-            this.cursor++;
-        }
-    }
-
-    _parseRule () : Rule {
-        // Is it a At-Rule, or is just a CSS rule?
-        // If it is a CSS rule, parse it
-        // If it is a At-Rule, call the correspondent parser.
-        if (this.rules[this.cursor] === '@') {
-            let atRuleName = this._parseTheNameOfAtRule();
-            this._skipWhitespacesAndBreaks();
-            
-            switch (atRuleName) {
-                case '@media':
-                case '@support':
-                    const start = this.cursor;
-                    this._moveCursorUntil(ch => ch === '{');
-                    const conditions = this.rules.slice(start, this.cursor);
-                    
-                    this.cursor++;
-                    const rules = this._parseRuleSet();
-
-                    return {
-                        type: atRuleName,
-                        conditions,
-                        rules
-                    };
-                case '@keyframes':
-                    const animationName = this._parseKeyframesIdent();
-                    const keyframesDeclr = this._parseKeyframesDeclr();
-                    return {
-                        type: '@keyframes',
-                        name: animationName,
-                        declr: keyframesDeclr
-                    }
-                default:
-                    throw new Error([
-                        `The At-Rule "${atRuleName}" is not supported.` +
-                        `Currently supported At-Rules are @media, @support and @keyframes.`
-                    ].join('\n'));
+                listOfDeclr.push(declr);
             }
+        } else if (rule.type === '@keyframes') {
+            keyframesMap.set(rule.name, rule);
         } else {
-            return this._parseCSSRule();
+            // '@media' && '@support'
+            addScope(scope, rule.rules);
         }
     }
+}
 
-    private _moveCursorUntil (shouldStop : (ch : string) => boolean) : void {
-        while (this.cursor < this.rules.length && !shouldStop(this.rules[this.cursor])) {
-            this.cursor++;
-        }
-    }
+function getAnimationName (str : string) : string {
+    let animationValues : string[] = str.split(' ');
+    let hasFoundValueFor : { [prop : string] : boolean };
 
-    private _parseTheNameOfAtRule () : string {
-        let start : number = this.cursor;
-        this._moveCursorUntil(this._isWhitespaceOrBreak.bind(this));
-        return this.rules.slice(start, this.cursor);
-    }
+    // Define a map that save all possible keywords.
+    let mapTypeOfKeyword = new Map([
+        [ 'ease', 'timing-function' ],
+        [ 'ease-in', 'timing-function' ],
+        [ 'ease-out', 'timing-function' ],
+        [ 'ease-in-out', 'timing-function' ],
+        [ 'linear', 'timing-function' ],
+        [ 'step-start', 'timing-function' ],
+        [ 'step-end', 'timing-function' ],
+        [ 'initial', 'timing-function' ],
+        [ 'unset', 'timing-function' ],
+        [ 'inherit', 'timing-function' ],
+        [ 'normal', 'direction' ],
+        [ 'reverse', 'direction' ],
+        [ 'alternate', 'direction' ],
+        [ 'alternate-reverse', 'direction' ],
+        [ 'none', 'fill-mode' ],
+        [ 'forwards', 'fill-mode' ],
+        [ 'backwards', 'fill-mode' ],
+        [ 'both', 'fill-mode' ],
+        [ 'running', 'play-state' ],
+        [ 'pause', 'play-state'],
+        [ 'infinite', 'iteration-count' ]
+    ]);
 
-    private _parseKeyframesIdent () : string {
-        const ESCAPE = '\\';
-        let END_CHAR; // The char shows the end of the keyframes ident.
-        let currentChar = this._getCurrentChar();
-        let start = this.cursor;
-
-        if (currentChar === '"' || currentChar === '\'') {
-            // Situation One: the identification is quoted. e.g. @keyframes "fade out" { ... }
-            END_CHAR = currentChar;
-            this.cursor++; // skip the start quote.
-        } else {
-            // Situation Two: the identification is not quoted. e.g. @keyframes fade-out { ... }
-            END_CHAR = ' ';
-        }
-
-        while (true) {
-            currentChar = this._getCurrentChar();
-            if (currentChar === ESCAPE) {
-                // In CSS, characters can be escaped by the '\' character.
-                // If we meet that character, we will know the next character
-                // is escaped, and they will never be the end char. 
-                // so we can simply skip them together.
-                this.cursor += 2;
-            } else if (currentChar === END_CHAR) {
-                break;
-            } else {
-                this.cursor++;
-            }
-        }
-
-        if (currentChar === '"' || currentChar === '\'') {
-            this.cursor++; // Skip the end quote.
-        }
-
-        this._skipWhitespacesAndBreaks(); 
-
-        return this.rules.slice(start, this.cursor);
-    }
-
-    private _parseKeyframesDeclr() : string {
-
-    }
-
-    private _parseListOfSelectors () : string[] {
-        // Assumed we have skipped all whitespaces
-        // and return characters.
-        let selectors = [];
-
-        while (this._getCurrentChar() !== '{') {
-            let start = this.cursor;
-
-            // A ',' will shows up between two selectors, and
-            // a ';' will shows up at the end of the selector
-            // list.
-            while (this._getCurrentChar() !== ',' && this._getCurrentChar() !== '{') {
-                this.cursor++;
-            }
-
-            let sel = this.rules.slice(start, this.cursor);
-            selectors.push(sel.trim());
-        }
-
-        return selectors;
-    }
-
-    private _parseDecalarations () : { property: string, value: string }[] {
-        /**
-         * Assumed we have skipped all whitespaces and breaks.
-         * A decalaration is something like following example:
-         * `color : red;`
-         */
-        let decalarations : { property: string, value: string }[] = [];
-
-        while (this._getCurrentChar() !== '}') {
-            let property, value, start;
-
-            start = this.cursor;
-            this._moveCursorUntil(ch => ch === ':');
-            property = this.rules.slice(start, this.cursor).trim();
-
-            this.cursor++; // skip the colon
-            this._skipWhitespacesAndBreaks();
-
-            start = this.cursor;
-            this._moveCursorUntil(ch => ch === ';')
-            value = this.rules.slice(start, this.cursor).trim();
-
-            decalarations.push({
-                property,
-                value
-            });
-        }
-        return decalarations;
-    }
-
-    // FIXME
-    private _parseCSSRule () : CSSRule {
-        /**
-         * A CSS Rule can be seen as two parts, the selector group,
-         * and the declaration block.
-         */
-
-        this._skipWhitespacesAndBreaks();
-        const selectors = this._parseListOfSelectors();
-        
-        // After parsing selectors, the cursor should pause on a '{'.
-        // We need to skip it.
-        this.cursor++;
-
-        this._skipWhitespacesAndBreaks();
-        const declarations = this._parseDecalarations();
-
-        // After parsing decalarations, the cursor should pause on a '}'.
-        // We need to skip it.
-        this.cursor++;
-
-        return {
-            type: 'cssRule',
-            selectors: selectors,
-            decalrations: declarations
+    if (animationValues.length === 8) {
+        hasFoundValueFor = {
+            'duration': false,
+            'timing-function': false,
+            'delay': false,
+            'iteration': false,
+            'iteration-count': false,
+            'direction': false,
+            'fill-mode': false,
+            'play-state': false
         };
+    } else if (animationValues.length === 4) {
+        hasFoundValueFor = {
+            'duration': false,
+            'timing-function': false,
+            'delay': false,
+            'name': false
+        };
+    } else if (animationValues.length === 2) {
+        hasFoundValueFor = {
+            'duration': false,
+            'name': false
+        };
+    } else {
+        throw new Error('Illegal animation value');
     }
 
-    private _isWhitespaceOrBreak (ch : string) {
-        return ch === ' ' || ch === '\t' || ch === '\r' || ch === '\n';
+    for (let value of animationValues) {
+        const proplist = getApplicableProperties(value);
+        const LEN = proplist.length;
+
+        for (let i = 0; i < LEN; i++) {
+            const PROP = proplist[i];
+            if (PROP in hasFoundValueFor) {
+                // We need that property
+                if (hasFoundValueFor[PROP]) {
+                    // We have found another value for this property before.
+                    continue;
+                } else if (PROP === 'name') {
+                    return value;
+                } else {
+                    // We haven't found value for this property.
+                    // so we mark it as found.
+                    hasFoundValueFor[PROP] = true;
+                }
+            } else {
+                if (i < LEN - 1) {
+                    // We don't need that property, try next one.
+                    continue;
+                } else {
+                    // We try all applicable properties but none
+                    // of them are required. So we have to throw
+                    // an error.
+                    new Error('Illegal animation value');
+                }
+            }
+        }
     }
 
-    private _getCurrentChar () {
-        return this.rules[this.cursor];
+    /**
+     * Given a value, it returns all possible `animation-*` properties
+     * to which we can apply the value. There might be properties that
+     * we CAN apply the value to, but we SHOULDN'T DO THAT, since that
+     * property is not included in the shorthand, or its in the
+     * shorthand, but some other property has higher applying priority,
+     * "robbing" the value from it.
+     *
+     * For example:
+     * ```
+     * animation: 4s pause ease-in 1s
+     * ```
+     * The second value, `pause`, can be applied to `animation-play-state`
+     * and `animation-name`. By specification, `animation-play-state` is
+     * not included in a 4-values shorthand, so, it's actually the value
+     * of `animation-name`.
+     * 
+     * On the other hand, the first value, `4s`, can be applied to
+     * `animation-duration` as well as `animation-delay`. Again, by
+     * specification, the former one has higher priority and we should
+     * apply the value to it.
+     */
+    function getApplicableProperties (value : string) : string[] {
+        // Check if the value is a <time> first.
+        if (/^calc\(.+\)$/.test(value) || isTime(value)) {
+            // animation-duration: 1s;
+            // animation-duration: 100ms;
+            // animation-duration: calc(0.1s * 20);
+            return [ 'duration', 'delay' ];
+        }
+        
+        // If not, check if it is a <number>.
+        if (isNumberLiteral(value)) {
+            // iteration-count: 10;
+            return [ 'iteration-count' ];
+        }
+
+        // If not, check if it is a special function for `animation-timing-function`.
+        if (/^cubic-bezier\(.+\)$/.test(value) || /^steps\(.+\)$/.test(value)) {
+            return [ 'timing-function' ];
+        }
+
+        // If not, check if it is a special keyword for certain `animation-*` property.
+        let type : string | null = mapTypeOfKeyword.get(value);
+        if (typeof type === 'string') {
+            if (isForbiddenWord(value)) {
+                // animation-timing-function: unset;
+                return [ type ];
+            } else {
+                // animatin-iteration-count: infinite;
+                // animation-name: infinite;
+                return [ type, 'name' ];
+            }
+        } 
+
+        // If none of them are true, it will only be a value for `animation-name`
+        return [ 'name' ];
+    }
+
+    function isNumberLiteral (value : string) : boolean {
+        return !Number.isNaN(Number(value));
+    }
+
+    function isForbiddenWord (value : string) : boolean {
+        return value === 'none' || value === 'initial' || value === 'inherit' || value === 'unset';
+    }
+
+    // Determine if a value is a <time>, e.g. 250ms, 0.13e4ms, 1.5s.
+    function isTime (value : string) : boolean {
+        if (value.length > 2 && value.endsWith('ms')) {
+            return isNumberLiteral(value.slice(0, value.length - 2));
+        }
+
+        if (value.length > 1 && value.endsWith('s')) {
+            return isNumberLiteral(value.slice(0, value.length - 1))
+        }
+
+        return false;
     }
 }
 
-function assembleSelectorsAndRules(splited) {
+function assembleSelectorsAndRules(ruleSet : Rule[]) {
     const joinSplitedRule = ({ selectorGroup, declarationBlock }) => selectorGroup + ' ' + declarationBlock;
-    return splited.map(joinSplitedRule).join('\n');
+    return ruleSet.map(joinSplitedRule).join('\n');
 }
-
